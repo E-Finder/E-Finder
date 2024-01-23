c9323450a5b8f9ee554b77f29d06af1a3fb5f502
Vespertino <Vespertino@C09PC20.informatica.comercio>
2024-01-23 19:54:14 +0100

    Actualización Toolbar++

--- E-Finder/app/src/main/java/com/example/e_finder/MainActivity.java
+++ E-Finder/app/src/main/java/com/example/e_finder/MainActivity.java
@@ -12,9 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        toolbar.setTitle("");
-        setSupportActionBar(toolbar);
     }
 
 
