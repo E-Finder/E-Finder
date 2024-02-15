7b25b1ea728fd4121a3fbd741618b488de0ce4a4
estebanbm02 <estebanbernaldam@gmail.com>
2024-02-15 23:15:41 +0100

    Commit noche

---
+++ EFinder/app/src/main/java/com/example/efinder/ContactanosActivity.java
@@ -0,0 +1,11 @@
+package com.example.efinder;
+
+import android.os.Bundle;
+
+public class ContactanosActivity extends ToolbarActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.contactanos_layout);
+    }
+}
