f4414f5446413e5e726a119941d5779db18936cb
Santi <14santi3c@gmail.com>
2024-02-16 00:13:11 +0100

    xml apañao

--- EFinder/app/src/main/java/com/example/efinder/ResultadoLocalizacionActivity.java
+++ EFinder/app/src/main/java/com/example/efinder/ResultadoLocalizacionActivity.java
@@ -1,18 +1,10 @@
 package com.example.efinder;
 
 import android.os.Bundle;
-<<<<<<< HEAD
-import DAO.EventoDAO;
-import Model.Evento;
-
-import android.util.Log;
-import android.widget.LinearLayout;
-
-=======
->>>>>>> 28509d479cbee805b8fe3b54c264ea7aa107c680
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList; // Asegúrate de importar ArrayList
 import java.util.List;
 
 import Adapter.EventoAdapter;
@@ -21,14 +13,11 @@ import Model.Evento;
 
 public class ResultadoLocalizacionActivity extends ToolbarActivity {
 
-<<<<<<< HEAD
     private RecyclerView recyclerView;
     private EventoAdapter adapter;
     private List<Evento> eventos;
     private EventoDAO eventoDAO;
 
-=======
->>>>>>> 28509d479cbee805b8fe3b54c264ea7aa107c680
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,7 +25,6 @@ public class ResultadoLocalizacionActivity extends ToolbarActivity {
 
         setToolbarOnClicks();
 
-<<<<<<< HEAD
         // Inicializar la lista de eventos
         eventos = new ArrayList<>();
 
@@ -44,13 +32,6 @@ public class ResultadoLocalizacionActivity extends ToolbarActivity {
         recyclerView = findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         adapter = new EventoAdapter(eventos); // Pasar la lista vacía al adaptador por ahora
-=======
-        // Convertido a variable local ya que no se requiere acceso a estos campos fuera de onCreate
-        List<Evento> eventos = obtenerEventos();
-        RecyclerView recyclerView = findViewById(R.id.recyclerView);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        EventoAdapter adapter = new EventoAdapter(eventos);
->>>>>>> 28509d479cbee805b8fe3b54c264ea7aa107c680
         recyclerView.setAdapter(adapter);
 
         // Inicializar el DAO de Evento
@@ -65,6 +46,7 @@ public class ResultadoLocalizacionActivity extends ToolbarActivity {
     }
 
     private List<Evento> obtenerEventos() {
+        // Asegúrate de que esta función retorna efectivamente una lista de eventos desde el DAO
         return eventoDAO.listarEventos();
     }
 }
